9be8b09bbe4349da0e189e4f238dd4d9d9e76156
David Ko <dkoflow@gmail.com>
2019-02-02 10:26:32 -0800

    Created and customized seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,72 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Quiz.destroy_all
+
+
+PASSWORD = "supersecret"
+
+codecore_user = User.create(
+    name: "Educator",
+    email: "educator@gmail.com",
+    password: PASSWORD,
+    educator: true
+)
+
+10.times do
+    name = Faker::Name.name
+    email = "#{name.downcase}@example.com"
+    User.create(
+        name: name,
+        email: email,
+        password: PASSWORD
+    )
+end
+users = User.all
+
+
+5.times do 
+    created_at = Faker::Date.backward(365 * 5)
+    q = Quiz.create(
+        name: Faker::Hacker.say_something_smart,
+        created_at: created_at,
+        updated_at: created_at,
+        # user: users.sample
+        user: codecore_user
+    )
+
+    if q.valid?
+        2.times do
+        qtn = Question.new(
+            body: Faker::Hacker.say_something_smart,
+            created_at: created_at,
+            updated_at: created_at,
+            quiz_id: (q.sample).id
+        )
+        q.questions << qtn
+        if qtn.valid?
+            4.times do
+                a = Answer.create(
+                    body: Faker::Hacker.say_something_smart,
+                    created_at: created_at,
+                    updated_at: created_at,
+                    questions_id: qtn.id,
+                )
+                end
+            end
+        end
+    end
+end
+
+
+
+users = User.all
+questions = Questions.all
+answers = Answer.all
+
+puts "Created #{users.count} users"
+puts "Created #{questions.count} questions"
+puts "Created #{answers.count} answers"
+
