8d6380de555ededeba4a3f861de4621fffc06d6c
David Ko <dkoflow@gmail.com>
2019-02-01 18:49:12 -0800

    Initialized Welcome page controller, view and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :questions
-  resources :scores
-  resources :quizzes
-  resources :users
+  
+  root "welcome#index"
+  
+  resources :quizzes do
+    resources :questions do
+      resources :answers 
+
+    end
+  end
+      
+      
+  resources :users do
+    resources :scores
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
