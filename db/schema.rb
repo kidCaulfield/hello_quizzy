d7ebdd2008ae9c4fc3513c97b7b7113b289fcfc3
David Ko <dkoflow@gmail.com>
2019-02-02 13:21:33 -0800

    Added total column and renamed grade to points column in scores table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_013341) do
+ActiveRecord::Schema.define(version: 2019_02_02_211934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,10 @@ ActiveRecord::Schema.define(version: 2019_02_02_013341) do
   create_table "scores", force: :cascade do |t|
     t.bigint "quiz_id"
     t.bigint "user_id"
-    t.integer "grade"
+    t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total"
     t.index ["quiz_id"], name: "index_scores_on_quiz_id"
     t.index ["user_id"], name: "index_scores_on_user_id"
   end
