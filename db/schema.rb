2d0fe67c642a82a9524062a93bb9e5a3b33ec0ad
David Ko <dkoflow@gmail.com>
2019-02-02 14:42:15 -0800

    Edited Quizzes controller actions and added description column to Quizzes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_213024) do
+ActiveRecord::Schema.define(version: 2019_02_02_223203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_02_02_213024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "published", default: false
+    t.text "description"
     t.index ["user_id"], name: "index_quizzes_on_user_id"
   end
 
