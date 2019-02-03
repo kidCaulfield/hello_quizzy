4af99d1797407e93203a36438f5c0c3477f59aab
Wes Coderre <wesley.coderre@gmail.com>
2019-02-02 16:14:56 -0800

    started quiz in progress logic, question show controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,17 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only: [:show, :edit, :update, :destroy]
+  before_action :find_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
   # GET /questions.json
   def index
     @questions = Question.all
+
   end
 
   # GET /questions/1
   # GET /questions/1.json
   def show
+    @answers = @question.answers.order(created_at: :desc)
   end
 
   # GET /questions/new
@@ -62,13 +64,12 @@ class QuestionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_question
-      @question = Question.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
       params.require(:question).permit(:body, :quiz_id)
+    end
+
+    def find_question
+      @question = Question.find params[:id]
     end
 end
