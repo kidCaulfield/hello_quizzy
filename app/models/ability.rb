ef53abd4e4325aec3b0a57f2402a8a134e3c1c8c
Wes Coderre <wesley.coderre@gmail.com>
2019-02-03 13:45:53 -0800

    quiz create authorization bug fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,37 +11,21 @@ class Ability
       #   can :read, :all
       # end
     #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
-    alias_action(:create, :edit, :update, :delete, :destroy, to: :crud)
+
+    alias_action(:read, :create, :edit, :update, :delete, :destroy, to: :crud)
 
 
     can(:crud, Quiz) do |quiz|
-      quiz.user.eductor == true
+      quiz.user == user
     end
 
     can(:crud, Question) do |question|
-      question.user.eductor == true
+      question.user == user
     end
 
     can(:crud, Answer) do |answer|
-      answer.user.eductor == true
+      answer.user == user
     end
 
     can(:crud, User) do |user|
