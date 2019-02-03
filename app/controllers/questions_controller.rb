7218b4ba175fd6ebb8680737bdcb50fa8a318aa0
Ming Chai <ming.chai4@gmail.com>
2019-02-03 14:05:40 -0800

    question actions fully functional. Redirects for each go to appropriate spot

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,6 @@ class QuestionsController < ApplicationController
   before_action :find_question, only: [:show, :edit, :update, :destroy]
   # before_action :authorize_user!, only: [:edit, :update, :destroy]
 
-  # GET /questions
-  def index
-    @questions = Question.all.order(created_at: :desc)
-  end
-
   # GET /questions/1
   def show
     @answers = @question.answers.order(created_at: :desc)
@@ -29,7 +24,7 @@ class QuestionsController < ApplicationController
     
     if @question.save question_params
       flash[:success] = "Question created"
-      redirect_to quiz_questions_path(@question.quiz)
+      redirect_to quiz_path(@question.quiz)
     else
       flash[:danger] = "There appear to be some errors."
       render 'questions/new'
@@ -48,10 +43,8 @@ class QuestionsController < ApplicationController
   # DELETE /questions/1
   def destroy
     @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:danger] = "Question has been deleted"
+    redirect_to quiz_path(@question.quiz)
   end
 
   private
