e2f64372d3bd01f7edad478be51d5ab27d4e1bd3
Mark Glimm <markglimm@gmail.com>
2019-02-03 16:50:54 -0800

    Complete new answer action

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,9 +4,9 @@ class AnswersController < ApplicationController
 
   # GET /answers
   # GET /answers.json
-  def index
-    @answers = Answer.all
-  end
+  # def index
+  #   @answers = Answer.all
+  # end
 
   # GET /answers/1
   # GET /answers/1.json
@@ -16,50 +16,41 @@ class AnswersController < ApplicationController
   # GET /answers/new
   def new
     @answer = Answer.new
+    @answer.question = Question.find params[:question_id]
   end
 
   # GET /answers/1/edit
   def edit
+    @answer.question = Question.find params[:question_id]
   end
 
   # POST /answers
   # POST /answers.json
   def create
     @answer = Answer.new(answer_params)
-
-    respond_to do |format|
-      if @answer.save
-        format.html { redirect_to @answer, notice: 'Answer was successfully created.' }
-        format.json { render :show, status: :created, location: @answer }
-      else
-        format.html { render :new }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
-      end
+    @answer.question = Question.find params[:question_id]
+    if @answer.save
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /answers/1
   # PATCH/PUT /answers/1.json
   def update
-    respond_to do |format|
       if @answer.update(answer_params)
-        format.html { redirect_to @answer, notice: 'Answer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @answer }
+    
       else
-        format.html { render :edit }
-        format.json { render json: @answer.errors, status: :unprocessable_entity }
+
       end
-    end
   end
 
   # DELETE /answers/1
   # DELETE /answers/1.json
   def destroy
     @answer.destroy
-    respond_to do |format|
-      format.html { redirect_to answers_url, notice: 'Answer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
   end
 
   private
